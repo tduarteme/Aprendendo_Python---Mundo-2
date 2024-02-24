6554a1514961e4ec3435f43c13ee4b2acf5f03d5
Thiago Duarte <th7.duarte@gmail.com>
2024-02-24 04:50:57 -0300

    Exercícios Python

---
+++ EX45.py
@@ -0,0 +1,68 @@
+"""Crie um programa que faça o computador jogar Jokenpô com você."""
+
+from random import randint
+from time import sleep
+
+itens = ("PAPEL","PEDRA","TESOURA")
+print("""Suas opções: 
+      
+      [0] - PAPEL
+      [1] - PEDRA
+      [2] - TESOURA """)
+
+jogador = int(input("Informe a opção desejada: "))
+computador = randint(0,2)
+
+print("JO")
+sleep(1)
+print("KEN")
+sleep(2)
+print("PÔ")
+sleep(3)
+
+print("-=*"*20)
+
+if computador == 0: # Computador Jogou Papel.
+    if jogador == 0:
+        print ("EMPATE")
+
+    elif jogador == 1:
+        print ("COMPUTADOR venceu, escolheu {}".format(itens[computador]))
+        print ("Jogador jogou, {}" .format(itens[jogador]))
+
+    elif jogador == 2:
+        print ("JOGADOR venceu, escolheu {}".format(itens[jogador]))
+        print ("Computador jogou, {}" .format(itens[computador]))
+
+    else:
+        print ("JOGADA INVÁLIDA")
+
+
+elif computador == 1: # Computador Jogou, Pedra.
+    if jogador == 0:
+        print ("JOGADOR venceu, escolheu {}." .format(itens[jogador]))
+        print ("Computador perdeu, {}!" .format(itens[computador]))
+
+    elif jogador == 1:
+        print ("EMPATE")
+
+    elif jogador == 2:
+        print ("COMPUTADOR venceu, escolheu {}." .format(itens[computador]))
+        print ("Jogador perdeu, {}!" .format(itens[jogador]))
+
+elif computador == 2: # Computador Jogou, Tesoura.
+    if jogador == 0:
+        print ("COMPUTADOR venceu, escolheu {}.".format(itens[computador]))
+        print ("Jogador perdeu, {}!" .format(itens[jogador]))
+
+    elif jogador == 1:
+        print("JOGADOR venceu, escolheu {}." .format(itens[jogador]))
+        print ("Computador perdeu, {}!" .format(itens[computador]))
+
+    elif jogador == 2:
+        print ("EMPATE")
+
+    else:
+        print("JOGADA INVÁLIDA")
+
+print("-=*"*20)
