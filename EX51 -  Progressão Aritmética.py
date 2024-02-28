dbe8add1a04fc5215213414274d3290259be959c
Thiago Duarte <th7.duarte@gmail.com>
2024-02-28 19:30:59 -0300

    Exercícios Python

--- EX51 -  Progressão Aritmética.py
+++ EX51 -  Progressão Aritmética.py
@@ -1,3 +1,6 @@
+"""Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, 
+    mostre os 10 primeiros termos dessa progressão."""
+
 print("=-*"*20)
 print("10 TERMOS DE UMA PA")
 print("=-*"*20)
